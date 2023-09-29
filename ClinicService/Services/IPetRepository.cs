437aa1b80699ba8ec5f96bde2213a726ff7bb26b
Georgvit <ifreyd@mail.ru>
2023-09-30 01:28:41 +0300

    Add project files.

---
+++ ClinicService/Services/IPetRepository.cs
@@ -0,0 +1,10 @@
+﻿using ClinicService.Models;
+using System.Security.Cryptography;
+
+namespace ClinicService.Services
+{
+    public interface IPetRepository : IRepository<Pet, int> {
+        List<Pet>  GetByClientId(int id);
+
+    }
+}
